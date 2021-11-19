e56a57ff36a541f10f286867981e2ff7c940a13a
Saurav KumaR <chunnu.bihat@gmail.com>
2021-11-20 00:41:27 +0530

    done with user select mode(home page) #13

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import Home from './pages/home';
+import Single from './pages/single';
 
 class App extends Component {
 	
@@ -8,9 +9,9 @@ class App extends Component {
 		return (
 			<Router>
 				<Switch>
-					<Route path="/" component={Home}/>
-					{/* <Route path="/single" component={Single}/>
-					<Route path="/multi" component={Multi}/>
+					<Route exact path="/" component={Home}/>
+					<Route path="/single" component={Single}/>
+					{/* <Route path="/multi" component={Multi}/>
 					<Route path="/bot" component={Bot}/> */}
 				</Switch>
 			</Router>
