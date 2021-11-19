41e44acb39efec3d2255456319055234fffbf23c
Saurav KumaR <chunnu.bihat@gmail.com>
2021-11-19 22:26:36 +0530

    added mode buttons #13

--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Image, View, StyleSheet, Text, Touchable, TouchableOpacity, Dimensions, ImageBackground } from 'react-native';
-import board from '../assets/board.png';
+import board from '../assets/board_blur.png';
 
 export default class Home extends Component{
     constructor(props){
@@ -8,43 +8,72 @@ export default class Home extends Component{
     }  
 
     render () {
-        
         return (
-            <View>
-                <Image style={styles.image} blurRadius={10}>
-                    {/* <View style={styles.cntMode}>
+            <View style={styles.body}>
+                <ImageBackground style={styles.image}>
+                    <View style={styles.cntMode}>
                         <TouchableOpacity 
                             onPress={()=>{console.log('redirect to single player page')}}
+                            onMouseEnter
                             style={styles.btnMode}
                         >
-                            <Text>SinglePlayer</Text>
+                            <Text style={styles.txtMode}>Single Player</Text>
                         </TouchableOpacity>
-                    </View> */}
-                </Image>
+                        <TouchableOpacity 
+                            onPress={()=>{console.log('redirect to multi player page')}}
+                            style={styles.btnMode}
+                        >
+                            <Text style={styles.txtMode}>Multi Player</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity 
+                            onPress={()=>{console.log('redirect to bot play page')}}
+                            style={styles.btnMode}
+                        >
+                            <Text style={styles.txtMode}>Bot Play</Text>
+                        </TouchableOpacity>
+                    </View>
+                </ImageBackground>
             </View>
         );
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
+    body:{
+        backgroundColor: '#29292a',
+        height: Dimensions.get('window').height,
+        width: Dimensions.get('window').width,
     },
     image: {
         backgroundImage:`url(${board})`,
+        borderRadius: 25,
         flex: 1,
         position: 'absolute',
-        resizeMode: 'cover',
         marginHorizontal: 10,
         marginVertical: 50,
         width: 1822,
         height: 898
-      },
-    btnMode:{
-        backgroundColor: 'skyblue',
-        width: 500,
-        height: 100,
-        padding: 20,
-        textAlign: 'center'
     },
+    cntMode:{
+        flex: 1,
+        alignSelf: 'center',
+        justifyContent: 'center',
+        alignItems:'center',
+    },
+    btnMode:{
+        backgroundColor: 'blue',
+        width: 400,
+        height: 100,
+        textAlign: 'center',
+        borderRadius: 20,
+        borderColor: 'yellow',
+        borderWidth: 5,
+        marginVertical: 10
+    },
+    txtMode:{
+        fontSize: 50,
+        color: 'white',
+        padding: 10
+
+    }
 })
