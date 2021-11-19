a37a9d62074d8b44d07ac700126d5902c2b3700c
Saurav KumaR <chunnu.bihat@gmail.com>
2021-11-20 00:52:16 +0530

    #14 initialize board and players on console

---
+++ components/modeSelect.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { View, StyleSheet, Text, TouchableOpacity, Dimensions, ImageBackground } from 'react-native';
+import { Link } from 'react-router-dom';
+
+export default class ModeSelect extends Component{
+    constructor(props){
+        super(props);
+    }  
+
+    render () {
+        return (
+            <View style={styles.cntMode}>
+                <Link to='/single'>
+                    <TouchableOpacity 
+                        style={styles.btnMode}
+                    >
+                        <Text style={styles.txtMode}>Single Player</Text>
+                    </TouchableOpacity>
+                </Link>
+                <TouchableOpacity 
+                    style={styles.btnMode}
+                >
+                    <Text style={styles.txtMode}>Multi Player</Text>
+                </TouchableOpacity>
+                <TouchableOpacity 
+                    style={styles.btnMode}
+                >
+                    <Text style={styles.txtMode}>Bot Play</Text>
+                </TouchableOpacity>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    cntMode:{
+        flex: 1,
+        alignSelf: 'center',
+        justifyContent: 'center',
+        alignItems:'center',
+    },
+    btnMode:{
+        backgroundColor: 'blue',
+        width: 400,
+        height: 100,
+        textAlign: 'center',
+        borderTopLeftRadius: 20,
+        borderBottomRightRadius: 20,
+        borderColor: 'yellow',
+        borderWidth: 5,
+        marginVertical: 10
+    },
+    txtMode:{
+        fontSize: 50,
+        color: 'white',
+        padding: 10
+
+    }
+})
