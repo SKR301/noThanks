09cd006612c535b33b414857996ebfdd954a834f
Saurav KumaR <47807051+SKR301@users.noreply.github.com>
2021-11-23 23:59:56 +0530

    #18 player 2 cards/beads UI

--- components/player2.js
+++ components/player2.js
@@ -4,7 +4,7 @@ import { View, StyleSheet, Text, TouchableOpacity, Dimensions, ImageBackground }
 export default class Player2 extends Component{
     constructor(props){
         super(props);
-        this.state = { beads: 4, cards: [1,2,3,4,5], score: 0};
+        this.state = { beads: 4, cards: [1,2,3,4,5,6,7,8,9,0], score: 0};
     }  
 
     componentDidMount(){
@@ -14,7 +14,19 @@ export default class Player2 extends Component{
     render () {
         return (
             <View style={styles.container}>
-                <Text>Player2</Text>
+                <View style={styles.beadsContainer}>
+                    <Text style={styles.beads}>{this.state.beads}</Text>
+                </View>
+                <View style={styles.cardContainer}>
+                
+                    {
+                        this.state.cards.map((card) => (
+                            <View style={(this.state.cards.length > 6)?styles.cardFlexed:styles.cardUnflexed}>
+                                <Text key={card} style={styles.cardText}>{card}</Text>
+                            </View>
+                        ))
+                    }
+                </View>
             </View>
         );
     }
@@ -22,13 +34,51 @@ export default class Player2 extends Component{
 
 const styles = StyleSheet.create({
     container:{
-        // flex: 1,
-        // backgroundColor: 'red'
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center'
     },
-    currCardText:{
-        
+    cardContainer:{
+        flex: 7,
+        flexDirection: 'column',
+        marginTop: 100
+    },
+    cardUnflexed:{
+        height: 125,
+        borderColor: 'yellow',
+        backgroundColor: '#29292a',
+        borderWidth: 2,
+        borderRadius: 15,
+        marginTop: -50
+    },
+    cardFlexed:{
+        flex: 1,
+        borderColor: 'yellow',
+        backgroundColor: '#29292a',
+        borderWidth: 2,
+        borderRadius: 15,
+        marginTop: -50
+    },
+    cardText:{
+        flex: 1,
+        color: 'white',
+        padding: 15,
+        fontSize: 30
+    },
+    beadsContainer:{
+        flex: 3,
+        borderColor: 'dodgerblue',
+        borderWidth: 5,
+        borderRadius: 100,
+        height: 105,
+        width: 100,
+        marginHorizontal: 20,
+        alignSelf: 'center',
+        justifyContent: 'center',
     },
     beads:{
-        color: 'white'
+        color: 'white',
+        alignSelf: 'center',
+        fontSize: 50
     }
 })
